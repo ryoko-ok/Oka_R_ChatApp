87a47e095ab351860fd57c305150b99ec2fff9a8
Ryoko Oka <ryooo.kooo.22@gmail.com>
2021-02-19 22:24:06 -0500

    trying to set username

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ messenger.on('connection', (socket) => {
     // });
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', `${user.username} has joined the chat`);
+    // socket.broadcast.emit('message', `${user.username} has joined the chat`);
 
     socket.on('chatmessage', msg => {
         const user = getCurrentUser(socket.id);
@@ -69,4 +69,27 @@ messenger.on('connection', (socket) => {
         messenger.emit('message', `${user.username} has finished a break`);
 
     });
+});
+
+users = [];
+io.on('connection', function(socket) {
+   console.log('A user connected');
+   socket.on('setUsername', function(data) {
+      console.log(data);
+      if(users.indexOf(data) > -1) {
+         socket.emit('userExists', data + ' username is taken! Try some other username.');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+      }
+   });
+   
+   socket.on('msg', function(data) {
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+   })
+});
+
+http.listen(3000, function() {
+   console.log('listening on localhost:3000');
 });
